a0f4a7e50fc6d41c39fd7ab71fb0bd0a042add04
Xavion-git <xdean1@nait.ca>
2026-09-05 21:33:25 -0600

    Mixin OOP

---
+++ 01 - dart + flutter intro/ex02 - dart language exercises/student/bin/object.dart
@@ -0,0 +1,30 @@
+// This will be from the readings from the Flutter and Dart text book
+
+// Dart is an object-oriented programming language, 
+//
+// which means that it uses classes and objects to represent data and behavior. 
+// In Dart, everything is an object, including numbers, strings, and functions. 
+// This means that you can call methods on any value, and you can create your own 
+// classes to define your own types.
+
+
+// So in dart interfaces are defined by abstract classes, 
+// although there is no keyword for them.
+// and abstract classes are defined by the abstract keyword.
+  
+  
+  mixin Car { // So a mixin is a way to resue 
+  //            classes code in multiple class hierarchies.
+    void drive() {
+      print('Driving a car');
+    }
+
+    // the example is a car and cellphone doesnt have a logical class 
+    // but they can both mix in bluetooth connection.
+void main() {
+
+
+  }
+
+
+}
