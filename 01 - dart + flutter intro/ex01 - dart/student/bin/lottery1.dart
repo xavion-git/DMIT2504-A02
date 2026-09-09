6831ac2a3450a859b92bd7fc5f729ca0f49828b2
Xavion-git <xdean1@nait.ca>
2026-09-09 13:27:26 -0600

    Deduplications

--- 01 - dart + flutter intro/ex01 - dart/student/bin/lottery1.dart
+++ 01 - dart + flutter intro/ex01 - dart/student/bin/lottery1.dart
@@ -3,6 +3,10 @@
 // 3. I need some sort of function that returns a list of 6 ints, ranging 1-49 inclusively
 // 4. I need some sort of logic function that ensures no duplicates in the list (T
 
+// 5. Plainning for deduplacation:
+//  Option A) use ta set, loop until the set is {count} long (set auto deduplicates)
+// Option B) use a list, loop until the list is {count} long, check if the value is already in the list before adding it
+
 //2. command-line user inputs: I need to import dart:io module
 import 'dart:io';
 import 'dart:math'; 
@@ -27,11 +31,22 @@ void main() {
   List<int> lottoNumbers = List.filled(count, 0); // Count number of elements
 
    for (int i = 0; i < count; i++) {
+
     int value = rng.nextInt(largestNum) + 1; // .nextInt(upper range) print(i);
     
+    // Option A) check if value exists in the list already,
+    // if it does, decrement i to try again, else add the value to the list 
+    if (lottoNumbers.contains(value)) {
+      i--; // Decrement i to try again
+    } else {
+      lottoNumbers[i] = value; // assign the value to the list
+    }
+
     lottoNumbers[i] = value; // assign the value to the list
-    print(lottoNumbers[i]); // print the value to the console
+
+
    }
+   print(lottoNumbers.join(' '));
 }
 
 
